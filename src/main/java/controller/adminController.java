53b7a4521a4c95b0bf41a8faa21ef5e2165b3d73
Yura <LusYura@bigmir.net>
2017-02-05 16:40:21 +0200

    Initial_1

--- src/main/java/controller/adminController.java
+++ src/main/java/controller/adminController.java
@@ -2,6 +2,9 @@ package controller;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -9,6 +12,30 @@ import java.util.ResourceBundle;
 /**
  * Created by Comfy on 05.02.2017.
  */
-public class adminController  {
+public class adminController
+{
+    @FXML
+    private Label AdminLabel;
+
+    @FXML
+    private TextArea EmplArea;
+
+    @FXML
+    private TextArea InfoArea;
+
+    @FXML
+    private Button CreateButton;
+
+    @FXML
+    private Button ChenButton;
+
+    @FXML
+    private Button DelButton;
+
+    @FXML
+    private Button GenButton;
+
+
+
 
 }
