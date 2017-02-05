2b93e0e07b10213243174287ae5102e92e7ab820
AleksandrVyazovsky <san110489@gmail.com>
2017-02-05 15:50:14 +0200

    1

--- src/main/java/entity/Employer.java
+++ src/main/java/entity/Employer.java
@@ -4,4 +4,59 @@ package entity;
  * Created by Comfy on 05.02.2017.
  */
 public class Employer {
+    private String name;
+    private String sureName;
+    private Integer age;
+    private String sex;
+    private String position;
+
+
+    public Employer(String name, String sureName, Integer age, String sex, String position) {
+        this.name = name;
+        this.sureName = sureName;
+        this.age = age;
+        this.sex = sex;
+        this.position = position;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSureName() {
+        return sureName;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setSureName(String sureName) {
+        this.sureName = sureName;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
 }
--- src/main/java/entity/User.java
+++ src/main/java/entity/User.java
@@ -4,4 +4,40 @@ package entity;
  * Created by Comfy on 05.02.2017.
  */
 public class User {
+    private String login;
+    private String password;
+    private Employer employer;
+
+    public User(String login, String password, Employer employer) {
+        this.login = login;
+        this.password = password;
+        this.employer = employer;
+    }
+
+
+    public String getLogin() {
+        return login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public Employer getEmployer() {
+        return employer;
+    }
+
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setEmployer(Employer employer) {
+        this.employer = employer;
+    }
+
 }
