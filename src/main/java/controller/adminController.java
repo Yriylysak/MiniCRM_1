7086663a28b5656500d4bf240eb5c435584d4747
dmitry <d_arhont@i.ua>
2017-02-05 16:25:51 +0200

    окна

--- src/main/java/controller/adminController.java
+++ src/main/java/controller/adminController.java
@@ -1,7 +1,14 @@
 package controller;
 
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
 /**
  * Created by Comfy on 05.02.2017.
  */
-public class adminController {
+public class adminController  {
+
 }
--- src/main/java/controller/enterContorller.java
+++ src/main/java/controller/enterContorller.java
@@ -1,7 +1,34 @@
 package controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
 /**
  * Created by Yura on 05.02.2017.
  */
 public class enterContorller {
+    @FXML
+    private Label loginLabel;
+    @FXML
+    private Label passwordLabel;
+    @FXML
+    private TextField loginField;
+    @FXML
+    private TextField passwordField;
+    @FXML
+    private Button enterButton;
+    @FXML
+    private Button cancelButton;
+
+    @FXML
+    private void onActionSignIn() {
+        System.out.println(loginField.getText());
+        System.out.println(passwordField.getText());
+    }
+    @FXML
+    private void onActionCancel() {
+        System.exit(0);
+    }
 }
