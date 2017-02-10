30ac3d5e0336664eee0b5a2bdb9bab1ec3650fb1
JuliyaLypchuk <yuliya.lypchuk@gmail.com>
2017-02-10 16:19:19 +0200

    AdminController

--- src/main/java/controller/adminController.java
+++ src/main/java/controller/adminController.java
@@ -1,5 +1,10 @@
 package controller;
 
+import entity.Employer;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.Event;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -9,6 +14,8 @@ import javafx.scene.control.ListView;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
+import service.EmployerService;
+import service.EmployerServiceImpl;
 
 import java.io.IOException;
 
@@ -16,7 +23,7 @@ import java.io.IOException;
 /**
  * Created by Yura on 05.02.2017.
  */
-public class adminController {
+public class adminController implements EventHandler{
     @FXML  private ListView baseInfoList;
     @FXML  private TextField nameField;
     @FXML  private TextField surnameField;
@@ -29,22 +36,22 @@ public class adminController {
     @FXML  private Button DelButton;
     @FXML  private Button GenButton;
 
-    /*@FXML
-    private void onActionCreate() {
-        Parent root = null;
-        Stage stage = new Stage();
-        try{
-            root = FXMLLoader.load(getClass().getResource("/view/createEmployeeWindow.fxml"));
+    private EmployerService employerService = new EmployerServiceImpl();
+    private ObservableList<Employer> employerObservableList;
 
-        }catch (IOException e){e.printStackTrace();}
-        Scene scene = new Scene(root);
-        scene.getStylesheets().add("/view/enterWindow.css");
-        stage.setScene(scene);
-        stage.show();
-    }*/
     @FXML
     private void onActionCreate() {
-
+        employerObservableList = FXCollections.observableArrayList(employerService.findAll());
+        //EmplViewHandler viewHandler = new EmplViewHandler(nameField, surnameField, ageField, sexField, positionField, employerObservableList);
+        Employer employer = new Employer(nameField.getText(), surnameField.getText(),
+                (Integer.parseInt(ageField.getText())), sexField.getText(), positionField.getText());
+        employerService.add(employer);
+        baseInfoList.setItems(employerObservableList);
+        nameField.clear();
+        surnameField.clear();
+        sexField.clear();
+        ageField.clear();
+        positionField.clear();
     }
     @FXML
     private void onActionChen() {
@@ -54,5 +61,19 @@ public class adminController {
     }
     @FXML
     private void onActionGen() {
+    }
+
+    /*поки не працює*/
+    @Override
+    public void handle(Event event) {
+        EmployerService employerService = new EmployerServiceImpl();
+        ListView<Employer> employerListView = (ListView<Employer>) employerService.findAll();
+
+        Employer employer = employerListView.getSelectionModel().getSelectedItem();
+        nameField.setText(employer.getName());
+        surnameField.setText(employer.getSureName());
+        ageField.setText(Integer.toString(employer.getAge()));
+        sexField.setText(employer.getSex());
+        positionField.setText(employer.getPosition());
     }
 }
