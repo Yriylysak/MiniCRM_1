f5e5b7bddaa71922d278ad9160cad593df4fbc0c
dmitry <d_arhont@i.ua>
2017-02-15 10:51:26 +0200

    windows

---
+++ src/main/java/controller/CreateClientController.java
@@ -0,0 +1,40 @@
+package controller;
+
+import entity.Client;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+/**
+ * Created by dmitry on 15.02.17.
+ */
+public class CreateClientController {
+    @FXML
+    TextField fldName;
+    @FXML
+    TextField fldSurname;
+    @FXML
+    ListView clientList;
+    @FXML
+    Button btnAdd;
+    @FXML
+    Button btnCancelAdd;
+
+
+
+    @FXML
+    private void onActionAdd(){
+
+    }
+    @FXML
+    private void onActionCancelAdd(){
+        GraphicsLoader.closeWindow(btnCancelAdd);
+    }
+}
--- src/main/java/controller/ManagerController.java
+++ src/main/java/controller/ManagerController.java
@@ -1,7 +1,49 @@
 package controller;
 
+import enumTypes.OrderStatus;
+import javafx.collections.FXCollections;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 /**
  * Created by Comfy on 05.02.2017.
  */
 public class ManagerController {
+    @FXML TextField numberFld;
+    @FXML TextField managerFld;
+    @FXML TextField dateFld;
+    @FXML TextField priceFld;
+    @FXML DatePicker termFld;
+    @FXML TextField clientField;
+    @FXML Button btnNewClient;
+    @FXML private ComboBox<OrderStatus> combobox;
+
+    public void initialize(){
+        combobox.setPromptText("Выбрать");
+        combobox.setItems(FXCollections.observableArrayList(OrderStatus.values()));
+
+    }
+
+    public void onActionNewClient() {
+        Parent root = null;
+        Stage stage = new Stage();
+        try {
+            root = FXMLLoader.load(getClass().getResource("/view/createClient.fxml"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        Scene scene = new Scene(root);
+        scene.getStylesheets().add("/view/managerWindow.css");
+        stage.setScene(scene);
+        stage.show();
+        stage.setResizable(false);
+    }
 }
