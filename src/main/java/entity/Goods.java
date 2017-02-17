e58f93bdf1436c739dd5df26c0dd891145ee82d0
JuliyaLypchuk <yuliya.lypchuk@gmail.com>
2017-02-17 13:33:47 +0200

    entity Goods fixed.

--- src/main/java/entity/Goods.java
+++ src/main/java/entity/Goods.java
@@ -22,6 +22,8 @@ public class Goods {
     private Long availability;
 
 
+    public Goods() {
+    }
 
     public Goods(String productName, Double price /*Long availability*/) {
         this.productName = productName;
