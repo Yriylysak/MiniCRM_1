72d8db009711a8d0c12ae2dc012e7daefbdb0470
dmitry <d_arhont@i.ua>
2017-02-17 13:18:11 +0200

    окно товаров

--- src/main/java/controller/GoodsController.java
+++ src/main/java/controller/GoodsController.java
@@ -1,6 +1,9 @@
 package controller;
 
+import entity.Employee;
 import entity.Goods;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -11,6 +14,7 @@ import javafx.scene.control.CheckBox;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
+import util.ServiceUtil;
 
 import java.io.IOException;
 
@@ -32,12 +36,16 @@ public class GoodsController {
     CheckBox statusCheckY;
     @FXML
     CheckBox statusCheckN;
+    private ObservableList<Goods> goodsObservableList;
 
 
     @FXML
     private void onActionAddGoods(){
-       /* Goods goods = new Goods(fldGoodsName.getText(), fldGoodsPrice.getText());
-        ListView<Goods> goodsListView = */
+//        if (fldGoodsName.getText() != "" && fldGoodsPrice.getText() != "" && statusCheckY.getText()!= "" && statusCheckN.getText()!="") {
+//            Goods goods = new Goods(fldGoodsName.getText(), fldGoodsPrice.getText(), statusCheckY.getText(),statusCheckN.getText());
+//            ServiceUtil
+//        }
+//        goodsObservableList = FXCollections.observableArrayList(ServiceUtil.);
 
     }
     @FXML
