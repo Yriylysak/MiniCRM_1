c4e799e44b7abe3d4465e2385ee1ccd223d1a5fd
dmitry <d_arhont@i.ua>
2017-02-23 20:09:17 +0200

    profileWindow

---
+++ src/main/java/controller/ProfileController.java
@@ -0,0 +1,55 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+/**
+ * Created by dmitry on 23.02.17.
+ */
+public class ProfileController {
+    @FXML
+    TextField fldProfileName;
+    @FXML
+    TextField fldProfileSurname;
+    @FXML
+    TextField fldProfileEmail;
+    @FXML
+    TextField fldProfilePhone;
+    @FXML
+    TextField fldProfilePass;
+    @FXML
+    Button btnSaveEdit;
+    @FXML
+    Button btnEscProfile;
+
+    @FXML
+    private void onActionEsc() {
+        GraphicsLoader.closeWindow(btnEscProfile);
+        Parent root = null;
+        Stage primaryStage = new Stage();
+        try {
+            root = FXMLLoader.load(getClass().getResource("/view/enterWindow.fxml"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        primaryStage.setTitle("Авторизация");
+        Scene scene = new Scene(root);
+        scene.getStylesheets().add("/view/enterWindow.css");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+        primaryStage.setResizable(false);
+    }
+
+    @FXML
+    private void onActionSave(){
+
+    }
+}
